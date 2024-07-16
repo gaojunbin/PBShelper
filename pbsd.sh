44d01ef639ef42df4bb2b9a7aa7e48b492bacbf9
junbingao <gao.junbin.cn@gmail.com>
2024-07-16 18:05:15 +0800

    modify pbsd

--- pbsd.sh
+++ pbsd.sh
@@ -8,11 +8,27 @@ pbsd(){
     if [ -z "${dpbs_func_num}" ];then
         dpbs_func_num=1
     fi
-    if [ "${dpbs_func_num}" = 1 ];then
-        qstat
-        echo -e 'input the jobid you want to delete: \c'
-        read jobid
-        qdel ${jobid}
+    if [ "${dpbs_func_num}" = 1 ]; then
+        # 获取作业列表并添加序号列
+        job_list=$(qstat | awk 'NR > 2 { print NR-2, $0 }')
+        qstat | awk 'NR > 0 { if (NR-2 > 0) print NR-2, " ", $0; else if (NR-1 > 0) print "---", $0; else print "No.", $0 }'
+        if [ -z "${job_list}" ]; then
+            return
+        fi
+        # 提示用户输入要删除的作业序号
+        echo -e 'Input the Job No. you want to delete: \c'
+        read index
+
+        # 获取用户选择的作业 ID
+        jobid=$(echo "${job_list}" | awk -v idx=$index '$1 == idx { print $2 }')
+
+        # 删除选定的作业
+        if [ -n "${jobid}" ]; then
+            qdel "${jobid}"
+            print_text "green" "Job ${jobid} has been deleted." "false"
+        else
+            echo "Invalid job index or job ID."
+        fi
     elif [ "${dpbs_func_num}" = 2 ];then
         qstat
         echo -e 'input the jobid you want to force delete: \c'
