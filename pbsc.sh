c3dd01628787a120b4fa9614d30f290d3091ad61
junbingao <gao.junbin.cn@gmail.com>
2024-09-18 16:47:19 +0800

    update jupyterport

--- pbsc.sh
+++ pbsc.sh
@@ -86,11 +86,29 @@ cpbs_custom(){
     ssh_node=$(pwd)/ssh_node
     delete_all=$(pwd)/delete_all
 
-    if [ -z "$RmatePort" ]; then
-        local ssh_info="ssh \$(hostname)"
+    ports=("RmatePort" "-R" "JupyterPort" "-L")
+    local ssh_info="ssh \$(hostname)"
+
+    if [ -n "$ZSH_VERSION" ]; then
+        index_offset=1  # Zsh 从 1 开始
     else
-        local ssh_info="ssh -R ${RmatePort}:localhost:${RmatePort} \$(hostname)"
+        index_offset=0  # Bash 从 0 开始
     fi
+
+    # 遍历数组，根据不同的 shell 调整索引
+    for ((i=$index_offset; i<${#ports[@]}; i+=2)); do
+        port_name="${ports[$i]}"
+        port_flag="${ports[$((i+1))]}"
+        
+        # 使用 eval 获取环境变量的值
+        port_value=$(eval echo \$$port_name)
+
+        if [ -n "$port_value" ]; then
+            # 拼接 ssh_info，确保每个部分之间有空格，并使用正确的端口转发格式
+            ssh_info="$ssh_info $port_flag ${port_value}:localhost:${port_value}"
+        fi
+    done
+
 
     # generate the PBS script
     cat << EOF > ${job_name}.pbs
